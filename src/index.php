34f7b0cf62c71bb720691080384b80a65db4f14e
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-01-09 11:28:49 +0900

    変更

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>キャラクター特集</title>
+</head>
+<body>
+    <h1>アニメキャラクター管理サイト</h1>
+    <input type="button" onClick="document.location.href='list.php'" value="一覧"/>
+    <input type="button" onClick="document.location.href='insert_input.php'" value="登録"/>
+    <input type="button" onClick="document.location.href='update_input.php'" value="更新"/>
+    <input type="button" onClick="document.location.href='delete_input.php'" value="削除"/>
+</body>
+</html>
