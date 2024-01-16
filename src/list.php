4308c71990f266f93cd24e535df6bef92bde8ae1
Raratina956 <148516281+Raratina956@users.noreply.github.com>
2024-01-16 09:51:20 +0900

    d

--- src/list.php
+++ src/list.php
@@ -21,10 +21,28 @@
         <a href="index.php">トップへ戻る</a>
     </div>
     <hr><br>
+    <form action="list.php" method="post">
+        カテゴリー検索
     <?php
         $pdo=new PDO($connect,USER,PASS);
-        $sql=$pdo->query('select * from kyara');
+        $sql=$pdo->query('select * from title');
+        echo 'タイトル：<select name="title">';
+        echo '<option value=""></option>';
         foreach($sql as $row){
+            echo '<option value="', $row['title_name'] ,'">', $row['title_name'] ,'</option>';
+        }
+        echo '<input type="submit" value="検索"><br>';
+        if(isset($_POST['keyword'])){
+            $sql4=$pdo->prepare('select * from title where title_name=?');
+            $pdo->execute([$_POST['titile']]);
+            foreach($sql4 as $row3){
+                $sql3=$pdo->prepare('select * from kyara where title_id=?');
+                $pdo->execute([$_row3['title_id']]);
+            }
+        }else{
+            $sql3=$pdo->query('select * from kyara');
+        }
+        foreach($sql3 as $row){
             $sql2=$pdo->prepare('select * from title where title_id=?');
             $sql2->execute([$row['title_id']]);
             echo $row['kyara_id'], '：';
@@ -36,5 +54,6 @@
             echo '<br><br>';
         }
     ?>
+    </form>
 </body>
 </html>
